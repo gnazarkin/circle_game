8b145329f5f42812aba754d174b030816dfab50b
gnazarkin <gleb.nazarkin@gmail.com>
2014-07-09 21:51:39 -0400

    Random movement and sizes within the box

--- game.js
+++ game.js
@@ -1,17 +1,54 @@
+function Circle(){
+	this.x = Math.random() * 450;
+	this.y = Math.random() * 450;
+	this.diameter = 30 + Math.random() * 50;
+	this.speed = 500 + Math.random() * 1500;
+
+	this.render = function() {
+		this.$me = $('<div class="circle"></div>')
+			.css('left', this.x)
+			.css('top', this.y)
+			.css('height', this.diameter)
+			.css('width', this.diameter)
+
+		$('#game').append(this.$me);
+	};
+
+	this.move = function() {
+		var _this = this;
+		this.$me.animate({
+			top: Math.random() * 450,
+			left: Math.random() * 450
+		}, {
+			duration: this.speed,
+			complete: function() {
+				_this.move();
+			}
+		});
+	};
+
+	this.kill = function() {
+		this.$me.css('background-color','red')
+			.effect({
+				effect: 'explode',
+				duration: 100,
+				complete: function() {
+					$(this).remove();
+				},
+				queue: false
+			})
+	}
+}
+
 $(document).ready(function(){
 
-	$('.circle').animate({
-		left: "+300",
-		top: "+=300"
-	}, {
-		complete: function(){
-			alert("i moved!");
-		}
-	})
-	
-	
+	var circles = [];
 
+	for (var i = 0; i < 10; i++){
+		circles.push(new Circle());
+		circles[i].render();
+		circles[i].move();
+	}
 
-	
 })
 
